14ec5ae2436286939be007df6cd7b2ce85f589b5
phaneesh <paneeshhj@gmail.com>
2019-04-30 18:49:23 +0530

    homepage and profilepage refactored for performance

--- homepage/script/jsonresp.js
+++ homepage/script/jsonresp.js
@@ -1,148 +1,100 @@
-var promise = fetch("https://fsd1.herokuapp.com/users/1/details");
-	promise.then(response => response.json())
-		.then(d => updateDOM(d.data))
-		.catch(error => console.log(error));
+var urls = ["https://fsd1.herokuapp.com/users/1/details",
+"https://fsd1.herokuapp.com/users/1/followers/suggestions",
+"https://fsd1.herokuapp.com/users/1/following/tweets"];
 
-var promise = fetch("https://fsd1.herokuapp.com/users/1/followers/suggestions");
-	promise.then(response => response.json())
-	.then(d => updateSuggestions(d.data))
-	.catch(error => console.log(error));
+var consolidate = urls.map(url => fetch(url).then(res => res.json()));
+Promise.all(consolidate).then(res => acceptURLs.call(res));
 
-var promise = fetch("https://fsd1.herokuapp.com/users/1/following/tweets");
-	promise.then(response => response.json())
-	.then(d => tweetContent(d.data))
-	.catch(error => console.log(error));
-
-	var updateDOM = function(data) {
-	//console.log(data);
-	var bannerPic = document.querySelector("div.banner img");
-	bannerPic.src = data.cover_img;
-
-	var userName = document.querySelector("span.USERNAME");
-	userName.innerHTML = data.full_name;
-
-	var handle = document.querySelector("span.HANDLE");
-	handle.innerHTML = "@" + data.user_name;
-
-	var tweet = document.querySelector("div.followingInfo div:nth-child(1) span:nth-child(2)");
-	tweet.innerHTML = data.stats.tweets;
-
-	var Following = document.querySelector("div.followingInfo div:nth-child(2) span:nth-child(2)");
-	Following.innerHTML = data.stats.following;
-
-	var Followers = document.querySelector("div.followingInfo div:nth-child(3) span:nth-child(2)");
-	Followers.innerHTML = data.stats.followers;
-
-	var profileImg = document.querySelector("div.profilepic img");
-	profileImg.src = data.profile_img;
+var acceptURLs = function() {
+	updateDOM.apply(this[0]);
+	updateSuggestions.apply(this[1]);
+	tweetContent.apply(this[2]);
 }
 
-var updateSuggestions = function(data) {
-	var usernameList = document.querySelectorAll("#username");
-	for(var i = 0; i < usernameList.length; i++) {
-		usernameList[i].innerHTML = data[0].full_name;
-	}
-
-	var handleList = document.querySelectorAll("#handle");
-	for(var i = 0; i < usernameList.length; i++) {
-		handleList[i].innerHTML = "@" + data[0].user_name;
-	}
-
-	for(var i = 0; i < data.length && i < 3; i++) {
-		var leftmidmid = document.querySelector(".leftmidmid");
-		var div = document.createElement("div");
-		div.className = "smallicons";
-		var midmid = leftmidmid.appendChild(div);
-
-		var divPpimage = document.createElement("div");
-		divPpimage.className = "ppimage";
-		midmid.appendChild(divPpimage);
-		divPpimage.appendChild(document.createElement("img"));
-		var ppimage = document.querySelectorAll(".ppimage > img");
-		ppimage[i].src = data[i].profile_img;
-
-		var spandiv = div.appendChild(document.createElement("div"));
-		var span1 = spandiv.appendChild(document.createElement("span"));
-		span1.id = "username";
-		var username = document.querySelector("#username");
-		username.innerHTML = data[i].full_name;
-		var span2 = spandiv.appendChild(document.createElement("span"));
-		span2.id = "handle";
-		var handle = document.querySelector("#handle");
-		handle.innerHTML = "@" + data[i].user_name;
-
-		//spandiv.appendChild(document.createElement("br"));
-		var button = document.createElement("button");
-		button.id = "smallFollowButton";
-		spandiv.appendChild(button);
-		button.innerHTML = "Follow";
-	}
+var updateDOM = function() {
+	//console.log(this.data.user_name);
+	var data = this.data;
+	querySelectorImg("div.banner img", data.cover_img);
+	querySelectorInnerHTML("span.USERNAME", data.full_name);
+	querySelectorInnerHTML("span.HANDLE", "@" + data.user_name);
+	querySelectorInnerHTML("div.followingInfo div:nth-child(1) span:nth-child(2)", data.stats.tweets);
+	querySelectorInnerHTML("div.followingInfo div:nth-child(2) span:nth-child(2)", data.stats.following);
+	querySelectorInnerHTML("div.followingInfo div:nth-child(3) span:nth-child(2)", data.stats.followers);
+	querySelectorImg("div.profilepic img", data.profile_img);
 }
 
-var tweetContent = function(data) {
-	for(var i = 0; i < data.length; i++) {
-		var rightbottom = document.querySelector(".rightbottom");
-		var indposts = document.createElement("div");
-		indposts.className = "indposts";
-		rightbottom.appendChild(indposts);
-	
-		var ppimage = indposts.appendChild(document.createElement("div"));
-		ppimage.className = "ppimage";
-		indposts.appendChild(ppimage);
-		ppimage.appendChild(document.createElement("img"));
-		var profilepic = document.querySelector(".ppimage > img");
-		profilepic.src = data[i].user.profile_img;
-
-		var postcontent = indposts.appendChild(document.createElement("div"));
-		postcontent.className = "postcontent";
-		indposts.appendChild(postcontent);
-
-		var namehandle = postcontent.appendChild(document.createElement("div"));
-		namehandle.className = "namehandle";
-		postcontent.appendChild(namehandle);
-
-		namehandle.appendChild(document.createElement("span"));
-		namehandle.appendChild(document.createElement("span"));
-		namehandle.appendChild(document.createElement("span"));
-		namehandle.appendChild(document.createElement("span"));
-
-		let username = document.querySelectorAll(".namehandle span:nth-child(1)");
-		username[i].innerHTML = data[i].user.full_name;
-
-		let handle = document.querySelectorAll(".namehandle span:nth-child(2)");
-		handle[i].innerHTML = "@" + data[i].user.user_name;
-
-		var tweetbody = postcontent.appendChild(document.createElement("div"));
-		tweetbody.className = "tweetbody";
-		tweetbody.appendChild(document.createElement("span"));
-		tweetbody.innerHTML = data[i].text;
-
-		var postInteractions = postcontent.appendChild(document.createElement("div"));
-		postInteractions.className = "postInteractions";
-		postcontent.appendChild(postInteractions);
-
-		var comments = postInteractions.appendChild(document.createElement("div"));
-		comments.className = "comments";
-		postInteractions.appendChild(comments);
-		comments.appendChild(document.createElement("img"));
-		document.querySelector(".comments img").src = "img/replies.png";
-		comments.appendChild(document.createElement("span"));
-		document.querySelector(".comments span").innerHTML = data[i].stats.comments;
-
-		var likes = postInteractions.appendChild(document.createElement("div"));
-		likes.className = "likes";
-		postInteractions.appendChild(likes);
-		likes.appendChild(document.createElement("img"));
-		likes.querySelector(".likes img").src = "img/likes.png";
-		likes.appendChild(document.createElement("span"));
-		likes.querySelector(".likes span").innerHTML = data[i].stats.likes;
-
-		var retweets = postInteractions.appendChild(document.createElement("div"));
-		retweets.className = "retweets";
-		postInteractions.appendChild(retweets);
-		retweets.appendChild(document.createElement("img"));
-		retweets.querySelector(".retweets img").src = "img/retweets.png";
-		retweets.appendChild(document.createElement("span"));
-		retweets.querySelector(".retweets span").innerHTML = data[i].stats.retweets;
+var updateSuggestions = function() {
+	var data = this.data;
+	var div = '';
+	var leftmidmid = document.querySelector(".leftmidmid");
+	for(var i in data) {
+		var div = `<div class="smallicons">
+						<div class="ppimage">
+							<img src="${data[i].profile_img}">
+						</div>
+							<div>
+								<span id="username">
+									${data[i].full_name}
+								</span>
+								<span id="handle">
+									@${data[i].user_name}
+								</span>
+								<br>
+								<button id="smallFollowButton">Follow</button>
+							</div>
+						</div>
+					</div>` + div;
 	}
+	leftmidmid.innerHTML = div;
+}
+
+var tweetContent = function() {
+	var data = this.data;
+	var div = '';
+	var rightbottom = document.querySelector(".rightbottom");
+	for(var i in data) {
+		var div = `<div class="indposts">
+						<div class="ppimage">
+							<img src="${data[i].user.profile_img}"">
+						</div>
+						<div class="postcontent">
+							<div class="namehandle">
+								<span>${data[i].user.full_name}</span>
+								<span>@${data[i].user.user_name} &#8901</span>
+								<span>5m</span>
+								<span></span>
+							</div>
+							<div class="tweetbody">
+								<span>
+								${data[i].text}
+								</span>
+							</div>
+							<div class="postInteractions">
+								<div class="comments">
+									<img src="img/replies.png">
+									<span>${data[i].stats.comments}</span>
+								</div>
+								<div class="likes">
+									<img src="img/likes.png">
+									<span>${data[i].stats.likes}</span>
+								</div>
+								<div class="retweets">
+									<img src="img/retweets.png">
+									<span>${data[i].stats.retweets}</span>
+								</div>
+							</div>
+						</div>
+					</div>` + div;
+	}
+	rightbottom.innerHTML = div;
+}
+
+function querySelectorInnerHTML(target, data) {
+	var container = document.querySelector(target);
+	container.innerHTML = data;
+}
+
+function querySelectorImg(target, data) {
+	var container = document.querySelector(target);
+	container.src = data;
 }
--- profilepage/script/script.js
+++ profilepage/script/script.js
@@ -4,157 +4,138 @@ var urls = ["https://fsd1.herokuapp.com/users/1/details",
 "https://fsd1.herokuapp.com/users/1/following"];
 
 var consolidate = urls.map(url => fetch(url).then(res => res.json()));
-Promise.all(consolidate).then(data => updateDetails(data));//.then(res => console.log(res));
+Promise.all(consolidate).then(data => acceptURLs.call(data));//.then(res => console.log(res));
 
-function updateDetails(data) {
-	var profileImg = document.querySelector(".coverProfilePhoto img");
-	profileImg.src = data[0].data.profile_img;
+var acceptURLs = function() {
+	updateDetails.apply(this[0]);
+	tweetContent.apply(this[1]);
+	updateSuggestions.apply(this[2]);
+	followingContent.apply(this[3]);
+}
 
-	document.querySelector(".personalInfo span:nth-child(1)").innerHTML = data[0].data.full_name;
-	document.querySelector(".personalInfo span:nth-child(3)").innerHTML = "@" + data[0].data.user_name;
-	document.querySelector(".personalInfo span:nth-child(4)").innerHTML = data[0].data.user_bio;
+var updateDetails = function() {
+	var data = this.data;
+	querySelectorImg(".coverProfilePhoto img", data.profile_img);
+	querySelectorInnerHTML(".personalInfo span:nth-child(1)", data.full_name);
+	querySelectorInnerHTML(".personalInfo span:nth-child(3)", "@" + data.user_name);
+	querySelectorInnerHTML(".personalInfo span:nth-child(4)", data.user_bio);
 
-	document.querySelectorAll(".personalInfodata")[0].innerHTML = data[0].data.user_from;
-	document.querySelectorAll(".personalInfodata")[1].innerHTML = data[0].data.user_website;
-	document.querySelectorAll(".personalInfodata")[2].innerHTML = "Joined " + dataConv(data[0].data.user_created_at);
-	document.querySelectorAll(".personalInfodata")[3].innerHTML = dataConv(data[0].data.user_birthday);
+	querySelectorInnerHTMLAll(".personalInfodata", data.user_from, 0);
+	querySelectorInnerHTMLAll(".personalInfodata", data.user_website, 1);
+	querySelectorInnerHTMLAll(".personalInfodata", "Joined " + dataConv(data.user_created_at), 2);
+	querySelectorInnerHTMLAll(".personalInfodata", dataConv(data.user_birthday), 3);
 
-	document.querySelectorAll(".bannerMetaNumbers")[0].innerHTML = data[0].data.stats.tweets;
-	document.querySelectorAll(".bannerMetaNumbers")[1].innerHTML = data[0].data.stats.following;
-	document.querySelectorAll(".bannerMetaNumbers")[2].innerHTML = data[0].data.stats.followers;
+	querySelectorInnerHTMLAll(".bannerMetaNumbers", data.stats.tweets, 0);
+	querySelectorInnerHTMLAll(".bannerMetaNumbers", data.stats.following, 1);
+	querySelectorInnerHTMLAll(".bannerMetaNumbers", data.stats.followers, 2);
 
-	for(var i = 0; i < data[2].data.length && i < 3; i++) {
-		var leftmidmid = document.querySelector(".leftmidmid");
-		var div = document.createElement("div");
-		div.className = "smallicons";
-		var midmid = leftmidmid.appendChild(div);
+}
 
-		var divPpimage = document.createElement("div");
-		divPpimage.className = "ppimage";
-		midmid.appendChild(divPpimage);
-		divPpimage.appendChild(document.createElement("img"));
-		var ppimage = document.querySelectorAll(".ppimage > img");
-		ppimage[i].src = data[2].data[i].profile_img;
-
-		var spandiv = div.appendChild(document.createElement("div"));
-		var span1 = spandiv.appendChild(document.createElement("span"));
-		span1.id = "username";
-		var username = document.querySelector("#username");
-		username.innerHTML = data[2].data[i].full_name;
-		var span2 = spandiv.appendChild(document.createElement("span"));
-		span2.id = "handle";
-		var handle = document.querySelector("#handle");
-		handle.innerHTML = "@" + data[2].data[i].user_name;
-
-		//spandiv.appendChild(document.createElement("br"));
-		var button = document.createElement("button");
-		button.id = "smallFollowButton";
-		spandiv.appendChild(button);
-		button.innerHTML = "Follow";
+var updateSuggestions = function() {
+	var data = this.data;
+	var div = '';
+	var leftmidmid = document.querySelector(".leftmidmid");
+	for(var i in data) {
+		var div = `<div class="smallicons">
+						<div class="ppimage">
+							<img src="${data[i].profile_img}">
+						</div>
+							<div>
+								<span id="username">
+									${data[i].full_name}
+								</span>
+								<span id="handle">
+									@${data[i].user_name}
+								</span>
+								<br>
+								<button id="smallFollowButton">Follow</button>
+							</div>
+						</div>
+					</div>` + div;
 	}
+	leftmidmid.innerHTML = div;
+}
 
-	for(var i = 0; i < data[1].data.length; i++) {
-		var rightbottom = document.querySelector(".rightbottom");
-		var indposts = document.createElement("div");
-		indposts.className = "indposts";
-		rightbottom.appendChild(indposts);
-	
-		var ppimage = indposts.appendChild(document.createElement("div"));
-		ppimage.className = "ppimage";
-		indposts.appendChild(ppimage);
-		ppimage.appendChild(document.createElement("img"));
-		var profilepic = document.querySelector(".ppimage > img");
-		profilepic.src = data[1].data[i].user.profile_img;
-
-		var postcontent = indposts.appendChild(document.createElement("div"));
-		postcontent.className = "postcontent";
-		indposts.appendChild(postcontent);
-
-		var namehandle = postcontent.appendChild(document.createElement("div"));
-		namehandle.className = "namehandle";
-		postcontent.appendChild(namehandle);
-
-		namehandle.appendChild(document.createElement("span"));
-		namehandle.appendChild(document.createElement("span"));
-		namehandle.appendChild(document.createElement("span"));
-		namehandle.appendChild(document.createElement("span"));
-
-		let username = document.querySelectorAll(".namehandle span:nth-child(1)");
-		username[i].innerHTML = data[1].data[i].user.full_name;
-
-		let handle = document.querySelectorAll(".namehandle span:nth-child(2)");
-		handle[i].innerHTML = "@" + data[1].data[i].user.user_name;
-
-		var tweetbody = postcontent.appendChild(document.createElement("div"));
-		tweetbody.className = "tweetbody";
-		tweetbody.appendChild(document.createElement("span"));
-		tweetbody.innerHTML = data[1].data[i].text;
-
-		var postInteractions = postcontent.appendChild(document.createElement("div"));
-		postInteractions.className = "postInteractions";
-		postcontent.appendChild(postInteractions);
-
-		var comments = postInteractions.appendChild(document.createElement("div"));
-		comments.className = "comments";
-		postInteractions.appendChild(comments);
-		comments.appendChild(document.createElement("img"));
-		document.querySelector(".comments img").src = "img/replies.png";
-		comments.appendChild(document.createElement("span"));
-		document.querySelector(".comments span").innerHTML = data[1].data[i].stats.comments;
-		document.querySelector(".comments span").style.marginLeft = "5px";
-
-		var likes = postInteractions.appendChild(document.createElement("div"));
-		likes.className = "likes";
-		postInteractions.appendChild(likes);
-		likes.appendChild(document.createElement("img"));
-		likes.querySelector(".likes img").src = "img/likes.png";
-		likes.appendChild(document.createElement("span"));
-		likes.querySelector(".likes span").innerHTML = data[1].data[i].stats.likes;
-		document.querySelector(".likes span").style.marginLeft = "5px";
-
-		var retweets = postInteractions.appendChild(document.createElement("div"));
-		retweets.className = "retweets";
-		postInteractions.appendChild(retweets);
-		retweets.appendChild(document.createElement("img"));
-		retweets.querySelector(".retweets img").src = "img/retweets.png";
-		retweets.appendChild(document.createElement("span"));
-		retweets.querySelector(".retweets span").innerHTML = data[1].data[i].stats.retweets;
-		document.querySelector(".retweets span").style.marginLeft = "5px";
+var tweetContent = function() {
+	var data = this.data;
+	var div = '';
+	var rightbottom = document.querySelector(".rightbottom");
+	for(var i in data) {
+		var div = `<div class="indposts">
+						<div class="ppimage">
+							<img src="${data[i].user.profile_img}"">
+						</div>
+						<div class="postcontent">
+							<div class="namehandle">
+								<span>${data[i].user.full_name}</span>
+								<span>@${data[i].user.user_name} &#8901</span>
+								<span>5m</span>
+								<span></span>
+							</div>
+							<div class="tweetbody">
+								<span>
+								${data[i].text}
+								</span>
+							</div>
+							<div class="postInteractions">
+								<div class="comments">
+									<img src="img/replies.png">
+									<span>${data[i].stats.comments}</span>
+								</div>
+								<div class="likes">
+									<img src="img/likes.png">
+									<span>${data[i].stats.likes}</span>
+								</div>
+								<div class="retweets">
+									<img src="img/retweets.png">
+									<span>${data[i].stats.retweets}</span>
+								</div>
+							</div>
+						</div>
+					</div>` + div;
 	}
+	rightbottom.innerHTML = div;
+}
 
-	for(var i = 0; i < data[3].data.length; i++) {
-		var followingCards = document.querySelector(".followingCardsContainer").appendChild(document.createElement("div"));
-		followingCards.className = "followingCards";
-		var followingCardsDiv = followingCards.appendChild(document.createElement("div"));
-		
-		var followingCardsBanner = followingCardsDiv.appendChild(document.createElement("div"));
-		followingCardsBanner.className = "followingCardsBanner";
-		followingCardsDiv.appendChild(followingCardsBanner);
-		followingCardsBanner.appendChild(document.createElement("img"));
-		document.querySelector(".followingCardsBanner img").src = data[3].data[i].cover_img;
-		
-		var profilepic = followingCardsDiv.appendChild(document.createElement("div"));
-		profilepic.className = "profilepic";
-		profilepic.appendChild(document.createElement("img"));
-		document.querySelector(".profilepic img").src = data[3].data[i].profile_img;
-		
-		var cardMeta = document.createElement("div");
-		cardMeta.className = "cardMeta";
-		profilepic.appendChild(cardMeta);
-
-		var userMeta = document.createElement("div");
-		userMeta.className = "userMeta";
-		cardMeta.appendChild(userMeta);	
-		userMeta.appendChild(document.createElement("span"));
-		userMeta.appendChild(document.createElement("span"));	
-		var userBio = followingCards.appendChild(document.createElement("span"));
-		userBio.className = "userBio";
-		userBio.innerHTML = data[3].data[i].user_bio;
-
-		document.querySelector(".userMeta span:nth-child(1)").innerHTML = data[3].data[i].full_name;
-		document.querySelector(".userMeta span:nth-child(2)").innerHTML = "@" + data[3].data[i].user_name;
-		}
+var followingContent = function() {
+	var data = this.data;
+	var div = '';
+	var followingCards = document.querySelector(".followingCardsContainer");
+	for(var i in data) {
+		var div = `
+				<div class="followingCards">
+					<div>
+						<div class="followingCardsBanner"><img src="${data[i].cover_img}"></div>
+						<div class="profilepic">
+							<img src="${data[i].profile_img}">
+							<div class="cardMeta">
+								<div class="userMeta">
+									<span>${data[i].full_name}</span>
+									<span>@${data[i].user_name}</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<span class="userBio">${data[i].user_bio}</span>
+				</div>` + div;
 	}
+	followingCards.innerHTML = div;
+}
+
+function querySelectorInnerHTML(target, data) {
+	var container = document.querySelector(target);
+	container.innerHTML = data;
+}
+
+function querySelectorImg(target, data) {
+	var container = document.querySelector(target);
+	container.src = data;
+}
+
+function querySelectorInnerHTMLAll(target, data, index) {
+	var container = document.querySelectorAll(target)[index];
+	container.innerHTML = data;
+}
 
 function dataConv(UTCSeconds) {
 	var date = new Date(UTCSeconds);
@@ -164,3 +145,14 @@ function dataConv(UTCSeconds) {
 	var year = date.getFullYear();
 	return day + " " + month + " " + year;
 }
+
+var mediaSquare = function() {
+	var photosAndVideos = document.querySelector(".photosAndVideos");
+	div = '';
+	for(var i = 0; i < 9; i++) {
+		var div = `<div class="mediaSquare"></div>` + div;
+	}
+	photosAndVideos.innerHTML = div;
+}
+
+mediaSquare();
